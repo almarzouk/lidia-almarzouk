d40978ea2b26f94604cdb6b37d757092a9595ca8
jumaa almarzouk <84162824+almarzouk@users.noreply.github.com>
2025-03-27 07:08:30 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "img.kwcdn.com", "www.lidl.de","www.babyone.de","www.temu.com","www.amazon.de"],
+    domains: ["lh3.googleusercontent.com", "img.kwcdn.com", "www.lidl.de","www.babyone.de","www.temu.com","www.ebay.de"],
   },
 };
 
