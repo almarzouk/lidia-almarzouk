4eb081a9e89c378cd081b2ac99de8f1e789285bf
jumaa almarzouk <84162824+almarzouk@users.noreply.github.com>
2025-03-27 07:14:48 +0100

    Last update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "img.kwcdn.com", "www.lidl.de","www.babyone.de","www.temu.com","www.ebay.de","ebay.de","ebay.com","www.ebay.com","www.i.ebayimg.com","i.ebayimg.com","www.ebayimg.com","ebayimg.com","img-eu.kwcdn.com"],
+    domains: ["lh3.googleusercontent.com", "img.kwcdn.com", "www.lidl.de","www.babyone.de","www.temu.com","www.ebay.de","ebay.de","ebay.com","www.ebay.com","www.i.ebayimg.com","i.ebayimg.com","www.ebayimg.com","ebayimg.com","img-eu.kwcdn.com","m.media-amazon.com"],
   },
 };
 
